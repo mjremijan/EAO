62c3003430c189c7a98c49880be8ce8b8b0de190
Michael Remijan <mjremijan@yahoo.com>
2024-09-03 10:34:45 -0500

    Created "Mike meeting" to test with. Find just that meeting.

--- EAO/src/main/java/org/frb/h1mjr01/calendar/main/CalendarMain.java
+++ EAO/src/main/java/org/frb/h1mjr01/calendar/main/CalendarMain.java
@@ -5,9 +5,15 @@
 package org.frb.h1mjr01.calendar.main;
 
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 import net.fortuna.ical4j.data.CalendarBuilder;
 import net.fortuna.ical4j.model.Calendar;
-import net.fortuna.ical4j.model.Component;
 import net.fortuna.ical4j.model.component.VEvent;
 import net.fortuna.ical4j.model.property.Description;
 
@@ -19,62 +25,67 @@ public class CalendarMain {
     public static final void main(String [] args) throws Exception {
         System.out.printf("Starting CalendarMain%n");
         
+        Path path 
+            = Paths.get("\\\\rb.win.frb.org\\H1\\Accounts\\P-R\\H1MJR01\\Redirected\\Desktop\\Remijan Michael J Calendar.ics");
+        
         // Search and replace ##Empty.Group##
         {
-            // TODO
+            String content = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
+            content = content.replaceAll("##Empty.Group##", "frbgroup");
+            for (int i=1; i<=10; i++) {
+                try {
+                    System.out.printf("Try writing (%d)%n",i);
+                    Files.write(path, content.getBytes(StandardCharsets.UTF_8));
+                    i=11;
+                } catch (IOException e) {
+                    if (i==10) { 
+                        throw e;
+                    } else {
+                        Thread.sleep(10);
+                    }
+                }
+            }
         }
                 
         // Parse   
         System.out.printf("%nStarting parsing%n");
-        FileInputStream fin = new FileInputStream(
-            "\\\\rb.win.frb.org\\H1\\Accounts\\P-R\\H1MJR01\\Redirected\\Desktop\\Remijan Michael J Calendar.ics"
-        );
+        FileInputStream fin = new FileInputStream(path.toFile());
         CalendarBuilder builder = new CalendarBuilder();
         Calendar calendar = builder.build(fin);
         System.out.printf("Ending parsing%n");
         
         
-        // Test getComponents{}
-        System.out.printf("%nStrating test \"getComponents()\"%n");
-        for (Object o : calendar.getComponents()) {
-            Component c = (Component)o;
-            System.out.printf(
-                "Component name=\"%s\" classname=\"%s\" %n"
-                , c.getName(), c.getClass().getName()
-            );            
-            
-        }
+        // Mike meeting
+        System.out.printf("%nFinding \"Mike meeting\"%n");
+        VEvent mikeMeeting = findMikeMeeting(calendar);
+        System.out.printf(
+            "Mike Meeting%n -summary=\"%s\"%n -description=\"%s\"  %n"
+            , mikeMeeting.getSummary().get().getValue()
+            , mikeMeeting.getDescription().orElse(new Description("")).getValue()
+        );
         System.out.printf("Ending.%n");
         
         
-        // Test getComponents{"VEVENT}
-        System.out.printf("%nStrating test \"getComponents(\"VEVENT\")\"%n");
-        System.out.printf("-- ORIGINAL --%n");
-        for (Object o : calendar.getComponents("VEVENT")) {
-            VEvent e = (VEvent)o;
-            if (e.getSummary().isEmpty() == false) {
-                System.out.printf(
-                    "VEvent summary=\"%s\" description=\"%s\"  %n"
-                    , e.getSummary()
-                    , e.getDescription()
-                );  
-                e.replace(new Description("Hi Mike!"));
-            }
+        System.out.printf("%nExiting CalendarMain%n");
+    }
+
+    private static VEvent findMikeMeeting(Calendar calendar) {
+        List<VEvent> found 
+            = calendar.getComponents("VEVENT").stream()
+                .map (e -> (VEvent)e)
+                .filter(e -> e.getSummary().isEmpty() == false)
+                .filter(f -> f.getSummary().get().getValue().equals("Mike meeting"))
+                .collect(Collectors.toList())
+        ;
+        if (found.isEmpty()) {
+            throw new RuntimeException("\"Mike meeting\" not found");
         }
-        System.out.printf("-- REPLACED --%n");
-        for (Object o : calendar.getComponents("VEVENT")) {
-            VEvent e = (VEvent)o;
-            if (e.getSummary().isEmpty() == false) {
-                System.out.printf(
-                    "VEvent summary=\"%s\" description=\"%s\"  %n"
-                    , e.getSummary()
-                    , e.getDescription()
-                );  
-            }
+        else
+        if (found.size() != 1) {
+            throw new RuntimeException("More than 1 \"Mike meeting\" found");
+        } 
+        else {
+            return found.getFirst();
         }
-        System.out.printf("Ending.%n");
-        
-        
-        System.out.printf("Exiting CalendarMain%n");
     }
 }
